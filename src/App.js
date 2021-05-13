0d3141a33c2713a68c643e8ad42ac336557bd45c
Vatsal Khemani <43574951+vatsalkhemani@users.noreply.github.com>
2021-05-13 20:31:00 +0530

    nameadded

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState,useEffect} from 'react';
+import { Button,FormControl,InputLabel,Input } from '@material-ui/core';
+import Message from './Message';
 
 function App() {
+  const [input,setInput]=useState('');
+  const [messages,setMessages]=useState(['hi','bois']);
+  const [username,setUsername]=useState('');
+
+  //useState =variable in React
+  //useEffect=run code on a condition in ract
+  useEffect(()=>{
+    //run code here
+    //if [] is blank then this code runs once when the app compoent loads
+     //if we have a variable like input, this code runs whenever input changes
+     setUsername(prompt('Please enter you name'));
+  }, [])//condition
+
+  const sendMessage = (event)=> {
+    event.preventDefault();
+    setMessages([...messages,input]);
+    setInput('');
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello worlVatsald</h1>
+  <h2>Welcome {username}</h2>
+      <form>
+      <FormControl>
+        <InputLabel>Enter a message</InputLabel>
+        <Input value={input} onChange={event => setInput(event.target.value )} />
+        <Button disabled={!input} variant="contained" color="primary" type="submit" onClick={sendMessage}>Send Message</Button>
+      </FormControl>
+      </form>
+      {
+        messages.map(message => (
+          <Message text={message}/>
+        ))
+      }
     </div>
   );
 }
